3d23956656beca8b2c5ab05f697147112940c72b
akash <akasgselvam333@gmail.com>
2022-07-14 09:30:12 +0530

    changes

--- first/src/first/helloworld.java
+++ first/src/first/helloworld.java
@@ -1,6 +1,7 @@
 package first;
+
 public class helloworld {
-public static void main(String[] args){
-	 System.out.println("helloworld");
-	 }
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+	}
 }
