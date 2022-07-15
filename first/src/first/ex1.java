07f657aef29a0fa4675312c0ffb2bb4a150d6292
akash <akasgselvam333@gmail.com>
2022-07-15 08:57:03 +0530

    changes

---
+++ first/src/first/ex1.java
@@ -0,0 +1,13 @@
+package first;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		boolean isJavaFun = true;
+		boolean isFishTasty = false;
+		System.out.println(isJavaFun);     // Outputs true
+		System.out.println(isFishTasty);
+
+	}
+
+}
