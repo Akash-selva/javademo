2022285d2db150fe749769bdb8af2aa0e2988188
akash <akasgselvam333@gmail.com>
2022-07-19 15:18:22 +0530

    changes

---
+++ first/src/first/ex_12prepost.java
@@ -0,0 +1,26 @@
+package first;
+
+public class ex_12prepost {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i = 3;
+		
+		int a = i++;
+		
+		System.out.println("a = " + a);
+		System.out.println("i = " + i);
+		
+//		int b = i;
+		
+		String x = null;
+		if (x!=null) {
+			System.out.println("x has a value");
+		} else {
+			System.out.println("x does not have a value");
+		}
+
+		
+	}
+
+}
