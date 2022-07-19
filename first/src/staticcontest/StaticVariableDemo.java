356c7884bdebcc3b7040b89ef062aec985486efa
akash <akasgselvam333@gmail.com>
2022-07-19 17:46:51 +0530

    changes

---
+++ first/src/staticcontest/StaticVariableDemo.java
@@ -0,0 +1,13 @@
+package staticcontest;
+
+public class StaticVariableDemo {
+
+	
+		static int num;
+		
+		public static void main(String[] args) {
+			System.out.println("Static Variable value = " + StaticVariablesDemo.num);
+
+	}
+
+}
